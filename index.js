1ebe46af213a4a1d9daa6d2c32fb31ffb2602f3b
Varun Upadhyay <varun.802.vu@gmail.com>
2025-01-20 15:19:34 +0530

    another way of blocking hiding + (debouncing remaining)

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ let quote_author = "";
 const container = document.querySelector("#quote-container");
 const loader = document.querySelector(".loader");
 const getQuotes = async () => {
-    //start loader
-    loader.style.display = 'block'
-    container.style.display = 'none'
+    loader.classList.remove("hidden");
+    container.classList.add("hidden");
 
     const url = 'https://quotes15.p.rapidapi.com/quotes/random/?language_code=en';
     const options = {
@@ -24,16 +23,18 @@ const getQuotes = async () => {
         quote_author = result?.originator?.name || "Unknown author";
 
         const quote = document.getElementById('quote');
-        quote.innerHTML = quoteText ;
+        quote.innerHTML = quoteText;
         const author = document.getElementById('author');
-        author.innerHTML = quote_author ;
-        container.style.display = 'block'
-        loader.style.display = 'none'
-        
+        author.innerHTML = quote_author;
+
+        loader.classList.add("hidden");
+        container.classList.remove("hidden");
+
     } catch (error) {
         console.error(error);
-        loader.style.display = 'none'
-        container.style.display = 'block'
+
+        loader.classList.add("hidden");
+        container.classList.remove("hidden");
     }
 
 }
@@ -43,8 +44,10 @@ getQuotes();
 
 //clicking on a new quote should trigger the api again
 const newQuoteBtn = document.getElementById('new-quote');
-newQuoteBtn.addEventListener("click",()=>{
+newQuoteBtn.addEventListener("click", () => {
     getQuotes();
 
 })
+
+
 
