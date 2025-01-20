864ee5cdcce05aa80b83b2c9b934f7ed0492bf8b
Varun Upadhyay <varun.802.vu@gmail.com>
2025-01-20 15:11:39 +0530

    Loader Setup

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
 let quote = "";
 let quote_author = "";
+const container = document.querySelector("#quote-container");
+const loader = document.querySelector(".loader");
 const getQuotes = async () => {
+    //start loader
+    loader.style.display = 'block'
+    container.style.display = 'none'
+
     const url = 'https://quotes15.p.rapidapi.com/quotes/random/?language_code=en';
     const options = {
         method: 'GET',
@@ -21,12 +27,24 @@ const getQuotes = async () => {
         quote.innerHTML = quoteText ;
         const author = document.getElementById('author');
         author.innerHTML = quote_author ;
+        container.style.display = 'block'
+        loader.style.display = 'none'
+        
     } catch (error) {
         console.error(error);
+        loader.style.display = 'none'
+        container.style.display = 'block'
     }
 
 }
 
 getQuotes();
 
+
+//clicking on a new quote should trigger the api again
+const newQuoteBtn = document.getElementById('new-quote');
+newQuoteBtn.addEventListener("click",()=>{
+    getQuotes();
+
+})
 
